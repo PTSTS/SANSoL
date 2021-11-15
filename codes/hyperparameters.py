ca56ac0995b42e15af7cc976b406ab40b8683bb3
Peter Santos <AIMEPTSTSAM@mail.com>
2021-11-15 21:18:50 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -20,10 +20,10 @@ def obj_sansol(trial: optuna.trial.Trial):
     lies_path = f'{base_path}/mat_false_{rpns_rate}_{rpns_id}'
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
-        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/{lies_path.split('/')[-1]}""" \
+        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/{lies_path.split('/')[-1]} """ \
         f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOL --lies {lies_path} -save_results 1 """\
-        f"""--results /var/scratch/yan370/SANSOL/results.txt"""\
-        f"""--temp_results {key}.pkl"""
+        f"""--results /var/scratch/yan370/SANSOL/results.txt """\
+        f"""--temp_results {temp_results_path}"""
     cmd = subprocess.run(command, shell=True)
     results = pickle.load(open(temp_results_path, 'rb'))
     os.remove(temp_results_path)
