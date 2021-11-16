1897d70d9a462742b2a9e056a56e08c0e993e550
Peter Santos <AIMEPTSTSAM@mail.com>
2021-11-16 18:18:33 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -31,7 +31,7 @@ def obj_sansol(trial: optuna.trial.Trial):
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
         f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/SANSOL{lies_path.split('/')[-1]} """ \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOL --lies {lies_path}"""\
+        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOL --lies {lies_path} """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
@@ -51,7 +51,7 @@ def obj_sans(trial: optuna.trial.Trial):
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
         f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/SANS """ \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANS"""\
+        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANS """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
@@ -85,7 +85,7 @@ def obj_sansolf(trial: optuna.trial.Trial):
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
         f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/SANSOLF{lies_path.split('/')[-1]} """ \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOLF --lies {lies_path}"""\
+        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOLF --lies {lies_path} """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
