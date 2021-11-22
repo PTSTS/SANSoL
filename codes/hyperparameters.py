b161b6f74147275b9ebd91e6ba01ed8fdc730ae9
Peter Santos <AIMEPTSTSAM@mail.com>
2021-11-22 17:30:06 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -91,10 +91,10 @@ def obj_sansolf(trial: optuna.trial.Trial):
     os.remove(temp_results_path)
     return -results['MRR']
 
+
 def obj_pseudo(trial: optuna.trial.Trial):
     key = random.randint(0, 999999)
 
-    k_hop = trial.suggest_int('k', 2, 8)
     lr = trial.suggest_float('lr', 0.0001, 0.1)
     nss = trial.suggest_int('nss', 4, 256)
     b = trial.suggest_int('b', 16, 2048)
@@ -115,9 +115,8 @@ def obj_pseudo(trial: optuna.trial.Trial):
     lies_path = f'{base_path}/mat_false_{rpns_rate}_{rpns_id}'
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
-        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/pseudo{lies_path.split('/')[-1]} 
-""" \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns pseudo """\
+        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/pseudo{lies_path.split('/')[-1]}""" \
+        f""" --test_batch_size 16 -khop {0} --log_steps 100000 -ns pseudo """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
