f0ee17625c09d33298d8a1a63316ffb5ed14eb62
Peter Santos <AIMEPTSTSAM@mail.com>
2021-11-25 15:46:03 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -258,7 +258,7 @@ def obj_uniform(trial: optuna.trial.Trial):
     base_path = '/var/scratch/yan370/VLog'
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
-        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 """ \
+        f"""{nss} -b {b} -d 1000 -g 24.0 -a 1.0 -lr {lr} --max_steps 2656 -save models/uniform{key} """ \
         f""" --test_batch_size 16 -khop {0} --log_steps 100000 -ns uniform -save_results 0 """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
