ad69160f03aa96a3052ba1b51de32652854db48e
Peter Santos <AIMEPTSTSAM@mail.com>
2021-12-10 02:01:45 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -6,14 +6,22 @@ import random
 import subprocess
 
 
+hps = {
+    'SANSOL': {
+
+    }
+
+}
+
 def obj_sansol(trial: optuna.trial.Trial):
     key = random.randint(0, 999999)
 
     k_hop = trial.suggest_int('k', 2, 8)
     lr = 10 ** trial.suggest_float('log_lr', -8, -1)
-    gamma = trial.suggest_int('gamma', 0, 50)
+    gamma = 24
+
     nss = 256
-    b = trial.suggest_int('b', 16, 2048)
+    b = 1024
 
     # valid_paths = []
     # for filename in os.listdir('/var/scratch/yan370/VLog'):
@@ -30,7 +38,7 @@ def obj_sansol(trial: optuna.trial.Trial):
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
         f"""{nss} -b {b} -d 1000 -g {gamma} -a 1.0 -lr {lr} --max_steps 2656 -save models/SANSOL{lies_path.split('/')[-1]} """ \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOL --lies {lies_path} """\
+        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANSOL --lies {lies_path} --warm_up_steps 0"""\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
---
+++ codes/rpns_results.py
@@ -0,0 +1,77 @@
+key = 'SANSOLF4'
+mode = 'rpns'
+
+rpns_rates = [5, 10, 15, 20, 25, 32, 40, 50, 60, 70, 80, 90, 100]
+methods = ['SANSOL', 'SANSOLF', 'SANSOLcorr', 'SANSOLFcorr', 'RWSANSOL', 'RWSANSOLF', 'RWSANSOLcorr', 'RWSANSOLFcorr']
+with open('../../results/rpns.csv', 'r') as f:
+    lines = f.readlines()
+
+def get_result_array(method_str):
+    results_array = [[None for i in rpns_rates] for j in range(5)]
+    for line in lines:
+        if line:
+            result = line.split(sep='\t')
+            if result[0] == method_str:
+                if results_array[int(result[2])][rpns_rates.index(int(result[1]))] is not None:
+                    continue
+                else:
+                    results_array[int(result[2])][rpns_rates.index(int(result[1]))] = float(result[3])
+    return results_array
+
+
+
+
+if mode == 'rpns':
+    array = get_result_array(key)
+    for row in array:
+        print('\t'.join([str(x) for x in row]))
+    # print(results_array)
+
+elif mode == 'hops-ns':
+    for k in range(2, 9):
+        for ns in methods:
+            array = get_result_array(ns + str(k))
+            count = 0
+            total = 0
+            for row in array:
+                for cell in row:
+                    if cell is not None:
+                        total += cell
+                        count += 1
+
+            average = total / count if count != 0 else 0
+
+            print(average, end='\t')
+        print('')
+
+elif mode == 'rpns-ave':
+    full_array = [[0 for i in rpns_rates] for j in range(5)]
+    count_array = [[0 for i in rpns_rates] for j in range(5)]
+    for k in range(2, 9):
+        for ns in methods:
+            array = get_result_array(ns + str(k))
+            for i, row in enumerate(array):
+                for j, cell in enumerate(row):
+                    if cell is not None:
+                        full_array[i][j] += cell
+                        count_array[i][j] += 1
+
+    for i, row in enumerate(full_array):
+        for j, cell in enumerate(row):
+            if count_array[i][j] > 0:
+                print(cell / count_array[i][j], end='\t')
+        print('')
+
+if mode == 'rpns-k':
+    for k in range(2, 9):
+        rpns_total = [0 for x in rpns_rates]
+        rpns_counts = [0 for x in rpns_rates]
+        for ns in methods:
+            array = get_result_array(ns + str(k))
+            for i, row in enumerate(array):
+                for j, cell in enumerate(row):
+                    if cell is not None:
+                        rpns_total[j] += cell
+                        rpns_counts[j] += 1
+        k_average = [str(rpns_total[i] / rpns_counts[i]) if rpns_counts[i] != 0 else '0' for i in range(len(rpns_total))]
+        print('\t'.join(k_average))
