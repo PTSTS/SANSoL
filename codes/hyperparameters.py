2828b02ea94c0da3584756641a13b86674a6b1c1
Peter Santos <AIMEPTSTSAM@mail.com>
2021-12-10 14:47:33 +0100

    update

--- codes/hyperparameters.py
+++ codes/hyperparameters.py
@@ -60,7 +60,7 @@ def obj_sans(trial: optuna.trial.Trial):
     temp_results_path = f'{key}.pkl'
     command = f"""python -u codes/run.py --cuda --do_train --do_valid --data_path data/converted --model TransE -n """\
         f"""{nss} -b {b} -d 1000 -g {gamma} -a 1.0 -lr {lr} --max_steps 2656 -save models/SANS """ \
-        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANS  --warm_up_steps 0 """\
+        f"""--test_batch_size 16 -khop {k_hop} --log_steps 100000 -ns SANS  --warm_up_steps 0 --lies mat_false_60_1 """\
         f"""--temp_results {temp_results_path}"""
     cmd = subprocess.Popen(command, shell=True)
     out, err = cmd.communicate()
